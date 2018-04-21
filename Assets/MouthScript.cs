0f962a20b76653ca740bd7c0005017efa8e3cbef
Ivars Bergs <ivarsbergs@Ivarss-MacBook-Pro.local>
2018-04-21 19:42:41 +0300

    Add new screen shake

--- Assets/MouthScript.cs
+++ Assets/MouthScript.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouthScript : MonoBehaviour {
-	public ScoreScript scoreScript;
-	public string player;
-	GameObject cameraParent;
-	// Use this for initialization
-	void Start () {
-		cameraParent = GameObject.Find ("CameraParent");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class MouthScript : MonoBehaviour
+{
+    public ScoreScript scoreScript;
+    public string player;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	void OnTriggerEnter2D (Collider2D collision) {
-		Debug.Log (player);
-		if (collision.gameObject.tag.Equals("Food")) {
-			scoreScript.addScore (player, 50);
-			Destroy (collision.gameObject);
-			cameraParent.GetComponent<CameraShake> ().ShakeCamera(0.5f,0.25f);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(player);
+        if (collision.gameObject.tag.Equals("Food"))
+        {
+            scoreScript.addScore(player, 50);
+            Destroy(collision.gameObject);
+            Camera.main.gameObject.GetComponent<CameraShake>().Shake(0.3f);
+        }
+    }
 }
