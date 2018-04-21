d9bb18035e317354cd1e5558ad29e3c5a8a24976
taavi224 <taavi224@gmail.com>
2018-04-21 03:04:42 +0300

    Added PS3 movement support, random jiggle for forks

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour {
+	public string inputHorizontal;
+	public string inputVertical;
+	private float movementHorizontal;
+	private float movementVertical;
 	private int speed = 300;
 	private float velocity;
 	private Rigidbody2D rb;
@@ -14,8 +18,20 @@ public class MovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 movementDirection = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		Debug.Log (movementDirection);
+		if (Random.value < 0.5d) {
+			movementHorizontal = Input.GetAxis (inputHorizontal) + Random.value * 0.3f;
+		} else {
+			movementHorizontal = Input.GetAxis (inputHorizontal) - Random.value * 0.3f;
+		}
+		if (Random.value < 0.5d) {
+			movementVertical = Input.GetAxis (inputVertical) + Random.value * 0.3f;
+		} else {
+			movementVertical = Input.GetAxis (inputVertical) - Random.value * 0.3f;
+		}
+		Debug.Log (movementHorizontal);
+		Debug.Log(movementVertical);
+		Vector2 movementDirection = new Vector2 (movementHorizontal, movementVertical);
+
 		rb.velocity = movementDirection * speed * Time.deltaTime;
 	}
 }
