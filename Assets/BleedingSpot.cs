58feb544e0eb9b80f8a6997f835b2a820cd6868d
Ivars Bergs <ivarsbergs@Ivarss-MacBook-Pro.local>
2018-04-21 14:10:48 +0300

    Add bleeding from eyes

---
+++ Assets/BleedingSpot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BleedingSpot : MonoBehaviour
+{
+
+    public ParticleSystem BloodParticle;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D colider)
+    {
+        Debug.Log("Enter before check!" + colider.gameObject.tag);
+        if (colider.gameObject.tag == "Fork")
+        {
+            Debug.Log("Enter!");
+            this.BloodParticle.Play();
+        }
+    }
+}
